6a1b33737ef409ce6d7afdd216ba7c2315ff44cf
Mauricio <mauricioferrazsilva9@gmail.com>
2022-02-11 14:40:03 -0300

    Adicionar exemplos

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+public class program
+{
+    static int Adicionar20(int x)
+    {
+       return x + 20;
+    }
+    public static void Main()
+    {
+        int a =2;
+        a = Adicionar20(a);
+        WriteLine($"O valor da variavel e {a}");
+    }
+}    
+
